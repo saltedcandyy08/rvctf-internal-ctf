085e0d006317d179a399cf285dcbd4a26712ff67
sin <syu3887682@gmail.com>
2025-05-04 22:30:35 +0800

    made some changes to branson's challenges

--- crypto-challs/branson/easy/AES_nokey/weak_aes_challenge.py
+++ crypto-challs/branson/easy/AES_nokey/weak_aes_challenge.py
@@ -2,7 +2,7 @@ import os
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 
-flag = b'RVCTF{turns_out_you_can_brute_force_weak_AES_keys}'
+flag = b'RVCTF{REDACTED}'
 key = os.urandom(1) * AES.block_size  # Weak key, 1-byte repeated
 
 def encrypt(msg, key):
@@ -12,3 +12,5 @@ def encrypt(msg, key):
     return (iv + ciphertext).hex()
 
 print("Encrypted flag:", encrypt(flag, key))
+
+#Encrypted flag: d3c103b11b7dfd4fbc3cecd1521b339a9f11dbfae96bd1165e3365982e208d8f94f9f5fa4cb0fc22ef6426cf6017c5c8
