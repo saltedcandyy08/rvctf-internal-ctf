372e632b36a78001cae18bf220f80a2629789cc4
arjun-rvhs <162544764+arjun-rvhs@users.noreply.github.com>
2025-05-07 13:17:50 +0800

    Create food.py

---
+++ foren-challs/arjun/food.py
@@ -0,0 +1,10 @@
+# FOR KIM THE FATTY'S EYES ONLY
+
+import functools as _
+
+secret = "WYMY_YuYAFuD_uMD@uGAQl~i|x"
+
+def my_super_secret_function_that_no_one_can_expose_ha_ha_ha_river_valley_high_school_is_so_cooked(__):
+    ___ = lambda ____: chr(____ ^ (0b101010))
+    ____ = list(map(ord, list(__[::-1])))
+    return ''.join(map(___, ____))
